741ad098206e4cb45e5dc12b23860b5f906f512e
CMGS <ilskdw@mspil.edu.cn>
2012-03-15 11:41:04 +0800

    add more info

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ version = '0.1'
 
 setup(name='pool',
       version=version,
-      description="mysql connection pool for gevent",
+      description="mysql connection pool for gevent, split from sqlalchemy",
       long_description="""\
-split from sqlalchemy""",
+when we use gevent and pymysql, mysql connections would create and release in a request lifetime. that's why we need a pool to manage mysql connections.""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='gevent mysql sqlalchemy',
       author='CMGS',
