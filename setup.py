bdae5c14a26ba7f1dbc6a7f5b96b18a11a2f7be1
py <pengyu.py@gmail.com>
2016-07-08 17:21:38 +0800

    support py3k

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ when we use gevent and pymysql, mysql connections would create and release in a 
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
+          'six',
       ],
       entry_points="""
       # -*- Entry points: -*-
